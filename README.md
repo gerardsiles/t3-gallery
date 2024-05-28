51c5249d1abcf46afea0865864627eeff72f42fa
Gerard Siles <gerardsiles@gmail.com>
2024-05-28 14:31:50 +0000

    images to db

--- README.md
+++ README.md
@@ -7,8 +7,8 @@
 - [x] Make it deploy (Vercel)
 - [x] Scaffold basic ui with mock data
 - [x] Tidy up build process
-- [ ] Actually set up a database (vercel postgress)
-- [ ] Add Attach database to UI
+- [x] Actually set up a database (vercel postgress)
+- [x] Add Attach database to UI
 - [ ] Add Auth (w/ clerk)
 - [ ] Add image upload
 - [ ] Error management (w/ Sentry)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,17 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/e3efbb6c-a4e9-4cb2-b523-96518508737c-uzm0qg.jpg",
-  "https://utfs.io/f/ce5c9b37-962c-4648-a855-0e1ebf0e2e96-urjojl.jpg",
-  "https://utfs.io/f/a6e61f24-6780-4977-a36e-77c4e45bc88a-xhcvig.jpg",
-  "https://utfs.io/f/2da622d5-0d10-4772-b9b0-9bd47e755292-phu00g.jpg",
-  "https://utfs.io/f/392122b3-018b-481c-8d55-0eddfad638cd-1ay25c.jpg",
-  "https://utfs.io/f/4fa7373a-5418-4abe-b984-efba5a802717-t78mjc.jpg",
-];
-
-const mockedImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 export default async function HomePage() {
-  const post = await db.query.posts.findMany();
-  console.log(post);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {post.map((post, index) => (
-          <div key={post.id + "-" + index} className="w-48">
-            <p>{post.name}</p>
-          </div>
-        ))}
-        {mockedImages.map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} alt="image" />
+        {images.map((image, index) => (
+          <div key={image.id + "-" + index} className="flex w-48 flex-col">
+            <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
